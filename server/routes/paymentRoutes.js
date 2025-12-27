66fc5831001dbe1ad7c25b74de08c826dd3ae120
Mauro <maurobossio@outlook.com.ar>
2025-12-27 11:22:36 -0300

    Fix: Agregar postalCode requerido al crear orden de MP

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { createPreference, receiveWebhook } from '../controllers/paymentController.js';
+// 👇 1. IMPORTAR ESTO
 import checkAuth from '../middleware/auth.Middleware.js';
 
 const router = express.Router();
 
+// 👇 2. AGREGAR "checkAuth" AQUÍ (El portero de seguridad)
 router.post('/create-preference', checkAuth, createPreference);
 
-// RUTA NUEVA: Aquí MercadoPago nos avisará los cambios
 router.post('/webhook', receiveWebhook);
 
 export default router;
