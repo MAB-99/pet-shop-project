d0cda66ecf8e4b98d9d258e8467f547dcb2f19cc
Mauro <maurobossio@outlook.com.ar>
2025-12-27 10:21:24 -0300

    Feat: Implementacion de Webhook de MercadoPao 2

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { createPreference, receiveWebhook } from '../controllers/paymentController.js';
-import { authMiddleware } from '../middlewares/auth.Middleware.js';
+import checkAuth from '../middleware/auth.Middleware.js';
 
 const router = express.Router();
 
-router.post('/create-preference', authMiddleware, createPreference);
+router.post('/create-preference', checkAuth, createPreference);
 
 // RUTA NUEVA: Aquí MercadoPago nos avisará los cambios
 router.post('/webhook', receiveWebhook);
