316895610aeda5aa5a35928f0d03e7744d7f19d8
Mauro <maurobossio@outlook.com.ar>
2026-01-02 11:55:26 -0300

    Actualizacion de notificaciones

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -3,6 +3,8 @@ import {
     addOrderItems,
     getMyOrders,
     getAllOrders,
+    updateOrderToPaid,
+    updateOrderToDelivered,
     updateOrderStatus,
     getDashboardStats
 } from '../controllers/orderController.js';
@@ -15,5 +17,6 @@ router.get('/myorders', checkAuth, getMyOrders);
 router.get('/stats', checkAuth, getDashboardStats);
 router.get('/', checkAuth, getAllOrders);
 router.put('/:id/status', checkAuth, updateOrderStatus);
-
+router.put('/:id/paid', checkAuth, updateOrderToPaid);
+router.put('/:id/delivered', checkAuth, updateOrderToDelivered);
 export default router;
